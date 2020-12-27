e812e77d4817230e80b47fae8e25c3676260728c
Divesh Patel <djdivesh007@gmail.com>
2020-12-27 19:26:26 +0530

    Dashboard 3, 2 Changes

--- assets/js/demo/dashboard2.js
+++ assets/js/demo/dashboard2.js
@@ -1,3 +1,5 @@
+var sparkChart;
+
 $(function() {
 
     'use strict';
@@ -26,106 +28,15 @@ $(function() {
     // The Default colorPalette for this dashboard
     var colorPalette = ['#3f51b5', '#008FFB', '#FEB019', '#FF4560', '#775DD0']
 
-    var options = {
-        chart: {
-            type: 'line',
-            height: 280,
-        },
-        colors: colorPalette,
-        series: [{
-            name: 'sales',
-            data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-        }],
-        xaxis: {
-            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-        }
-    }
-
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();
-
-    /* Line, Column & Area */
-    var option4 = {
-        series: [{
-            name: 'Sales',
-            type: 'column',
-            data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30]
-        }, {
-            name: 'Purchases',
-            type: 'area',
-            data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43]
-        }, {
-            name: 'Profit',
-            type: 'line',
-            data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39]
-        }],
-        chart: {
-            height: 280,
-            type: 'line',
-            stacked: false,
-        },
-        colors: colorPalette,
-        stroke: {
-            width: [0, 2, 5],
-            curve: 'smooth'
-        },
-        plotOptions: {
-            bar: {
-                columnWidth: '50%'
-            }
-        },
-
-        fill: {
-            opacity: [0.85, 0.25, 1],
-            gradient: {
-                inverseColors: false,
-                shade: 'light',
-                type: "vertical",
-                opacityFrom: 0.85,
-                opacityTo: 0.55,
-                stops: [0, 100, 100, 100]
-            }
-        },
-        labels: ['01/01/2003', '02/01/2003', '03/01/2003', '04/01/2003', '05/01/2003', '06/01/2003', '07/01/2003',
-            '08/01/2003', '09/01/2003', '10/01/2003', '11/01/2003'
-        ],
-        markers: {
-            size: 0
-        },
-        xaxis: {
-            type: 'datetime'
-        },
-        yaxis: {
-            title: {
-                text: 'Points',
-            },
-            min: 0
-        },
-        tooltip: {
-            shared: true,
-            intersect: false,
-            y: {
-                formatter: function(y) {
-                    if (typeof y !== "undefined") {
-                        return y.toFixed(0) + " points";
-                    }
-                    return y;
-
-                }
-            }
-        }
-    };
-
-    var chart4 = new ApexCharts(document.querySelector("#line-column-area-chart"), option4);
-    chart4.render();
-
+ 
 
     var spark1 = {
         series: [{
+              name: 'Heart Beat',
             data: data.slice()
         }],
         chart: {
-            id: 'sparkline1',
+            id: 'realtime',
 
             type: 'area',
             height: 160,
@@ -139,59 +50,63 @@ $(function() {
                     speed: 1000
                 }
             },
+             toolbar: {
+            show: false
+          },
+          zoom: {
+            enabled: false
+          }
         },
-
+ dataLabels: {
+          enabled: false
+        },
         stroke: {
             curve: 'smooth'
         },
         fill: {
             opacity: 1,
         },
-        series: [{
-            name: 'Live',
-            data: randomizeArray(sparklineData)
-        }],
-        labels: [...Array(24).keys()].map(n => `2018-09-0${n+1}`),
+        // series: [{
+        //     name: 'Live',
+        //     data: randomizeArray(sparklineData)
+        // }],
+        // labels: [...Array(24).keys()].map(n => `2018-09-0${n+1}`),
         yaxis: {
-            min: 0,
+            //min: 0,
             max: 100,
+        },
+         legend: {
+          show: false
+        },
+         markers: {
+          size: 0
         },
 
         xaxis: {
             type: 'datetime',
-            // range: XAXISRANGE,
+             range: XAXISRANGE,
         },
         colors: [themeColors.primary],
-        title: {
-            text: 'Pulse Monitor',
-            offsetX: 20,
-            style: {
-                fontSize: '24px',
-                cssClass: 'apexcharts-yaxis-title'
-            }
-        },
-        subtitle: {
-            text: 'Critical',
-            offsetX: 20,
-            style: {
-                fontSize: '14px',
-                cssClass: 'apexcharts-yaxis-title'
-            }
-        }
+        // title: {
+        //     text: 'Pulse Monitor',
+        //     offsetX: 20,
+        //     style: {
+        //         fontSize: '24px',
+        //         cssClass: 'apexcharts-yaxis-title'
+        //     }
+        // },
+        // subtitle: {
+        //     text: 'Critical',
+        //     offsetX: 20,
+        //     style: {
+        //         fontSize: '14px',
+        //         cssClass: 'apexcharts-yaxis-title'
+        //     }
+        // }
     }
 
-    window.setInterval(function() {
-        getNewSeries(lastDate, {
-            min: 10,
-            max: 90
-        })
+   sparkChart = new ApexCharts(document.querySelector("#spark1"), spark1);
+    sparkChart.render();
 
-        chart.updateSeries([{
-            data: data
-        }])
-    }, 1000)
-
-
-    new ApexCharts(document.querySelector("#spark1"), spark1).render();
 
 });
